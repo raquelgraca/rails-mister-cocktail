4a83b0b6deedae13a3a30a0155640f89b2eef186
raquelgraca <raquelngraca@gmail.com>
2020-02-20 17:37:43 -0300

    Completed all the views

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -10,11 +10,10 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-    # we need `restaurant_id` to associate dose with corresponding cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to @cocktail
     else
       render :new
     end
